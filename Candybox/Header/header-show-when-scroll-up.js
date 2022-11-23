7fbb3fe27825deeb34400016b1b6d74c71265ca1
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-11-23 11:18:56 +0100

    :rocket: Launch header

--- Candybox/Header/header-show-when-scroll-up.js
+++ Candybox/Header/header-show-when-scroll-up.js
@@ -1,13 +1,15 @@
-const header = document.querySelector("header");
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".js-header");
 
-let last = 0;
-
-window.addEventListener("scroll", () => {
-    const current = window.scrollY;
-
-    // Check if last scroll is higher/lower than current scroll
-    last > current ? header.classList.remove(".hidden") : header.classList.add(".hidden");
-
-    // Assign current scroll to last scroll
-    last = current;
+    let last = 0;
+    
+    window.addEventListener("scroll", () => {
+        const current = window.scrollY;
+    
+        // Check if last scroll is higher/lower than current scroll
+        header ? last > current ? header.classList.remove("hidden") : header.classList.add("hidden") : null;
+    
+        // Assign current scroll to last scroll
+        last = current;
+    });
 });
