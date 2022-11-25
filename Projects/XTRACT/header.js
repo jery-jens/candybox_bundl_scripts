b54f0e08dac43741008db0eff2b386188a2c74fd
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2022-11-25 13:52:01 +0100

    Update header

--- Projects/XTRACT/header.js
+++ Projects/XTRACT/header.js
@@ -2,14 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const header = document.querySelector(".js-header");
 
     let last = 0;
-    
-    window.addEventListener("scroll", () => {
-        const current = window.scrollY;
-    
-        // Check if last scroll is higher/lower than current scroll
-        header ? last > current ? header.classList.remove("hidden-header") : header.classList.add("hidden-header") : null;
-    
-        // Assign current scroll to last scroll
-        last = current;
-    });
+
+    if (header) {
+        window.addEventListener("scroll", () => {
+            const current = window.scrollY;
+        
+            // Check if last scroll is higher/lower than current scroll
+            if (last > current) {
+                if (current <= 100) {
+                    header.classList.add("hidden-header");
+                } else {
+                    header.classList.remove("hidden-header");
+                };
+            } else {
+                if (current <= 100) {
+                    header.classList.add("hidden-header");
+                } else {
+                    header.classList.add("hidden-header");
+                };
+            };
+            
+            // Assign current scroll to last scroll
+            last = current;
+        });
+    };
 });
