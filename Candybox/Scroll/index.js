d459dd92eca34e5a15d454a5cfa4c562b242268c
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2023-01-12 10:22:09 +0100

    Update index.js

--- Candybox/Scroll/index.js
+++ Candybox/Scroll/index.js
@@ -2,7 +2,6 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     // loop: true,
     centeredSlides: true,
-    roundLengths: true,
     slidesPerView: "auto",
     spaceBetween: 10,
 });
